94f3ef4944ee1a1fa2fd39503ef6ea2a51110e00
abhinavrpatel <abhinavrpatel@gmail.com>
2017-08-17 16:21:51 -0700

    gas station problem

--- src/StringAndArray.java
+++ src/StringAndArray.java
@@ -854,4 +854,34 @@ public class StringAndArray {
         }
         return result.startsWith("0") ? result.substring(1) : result;
     }
+
+
+
+
+
+    /**
+     * There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+     * You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i
+     * to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+     * Return the starting gas station's index if you can travel around the circuit once, otherwise
+     * return -1.
+     */
+    public int gasStation(int[] gas, int[] cost) {
+        int gasLeft = 0;
+        int total = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            int remaining = gas[i] - cost[i];
+            if (gasLeft >= 0) {
+                gasLeft += remaining;
+            } else {
+                gasLeft = remaining;
+                start = i;
+            }
+            total += remaining;
+        }
+
+        return total < 0 ? -1 : start;
+    }
 }
