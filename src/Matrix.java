e8bad688caeaa14d28e89a095907ab1cca770772
abhinavrpatel <abhinavrpatel@gmail.com>
2017-08-25 13:27:13 -0700

    efficient matrix search 2

--- src/Matrix.java
+++ src/Matrix.java
@@ -175,4 +175,34 @@ public class Matrix {
         }
         return false;
     }
+
+
+
+
+
+
+    /**
+     * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has
+     * the following properties:
+     *     1) Integers in each row are sorted in ascending from left to right.
+     *     2) Integers in each column are sorted in ascending from top to bottom.
+     */
+    public static boolean efficientMatrixSearch2(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length - 1;
+
+        int i = m;
+        int j = 0;
+
+        while (i >= 0 && j <= n) {
+            if (target < matrix[i][j])
+                i--;
+            else if (target > matrix[i][j])
+                j++;
+            else
+                return true;
+        }
+
+        return false;
+    }
 }
