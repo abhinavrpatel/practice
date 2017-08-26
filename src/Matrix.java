bc2647016227e2b270f7be1d64c72e6be43847ad
abhinavrpatel <abhinavrpatel@gmail.com>
2017-08-26 13:21:08 -0700

    number of unique paths 1

--- src/Matrix.java
+++ src/Matrix.java
@@ -314,4 +314,36 @@ public class Matrix {
         }
         return cost[height - 1][width - 1];
     }
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * A robot is located at the top-left corner of a m x n grid. It can only move either
+     * down or right at any point in time. The robot is trying to reach the bottom-right
+     * corner of the grid. How many possible unique paths are there?
+     */
+    public static int numUniquePaths(int m, int n) {
+        int[][] cost = new int[m][n];
+        for (int i = 0; i < m; i++)
+            cost[i][0] = 1;
+
+        for (int i = 1; i < n; i++)
+            cost[0][i] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cost[i][j] = cost[i - 1][j] + cost[i][j - 1];
+            }
+        }
+
+        return cost[m - 1][n - 1];
+    }
 }
