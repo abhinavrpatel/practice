b28fa15a4437f69df39641251e7d118cf4402da3
abhinavrpatel <abhinavrpatel@gmail.com>
2017-08-26 16:21:34 -0700

    capture regions

--- src/Matrix.java
+++ src/Matrix.java
@@ -478,4 +478,54 @@ public class Matrix {
         }
         return i;
     }
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'. A region
+     * is captured by flipping all 'O's into 'X's in that surrounded region.
+     */
+    public static void captureRegions(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][0] == 'O')
+                mergeRegion(board, i, 0);
+            if (board[i][board[0].length - 1] == 'O')
+                mergeRegion(board, i, board[0].length - 1);
+        }
+
+        for (int i = 0; i < board[0].length; i++) {
+            if (board[0][i] == 'O')
+                mergeRegion(board, 0, i);
+            if (board[board.length - 1][i] == 'O')
+                mergeRegion(board, board.length - 1, i);
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                if (board[i][j] == 'M')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+
+    private static void mergeRegion(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O')
+            return;
+        board[i][j] = 'M';
+
+        mergeRegion(board, i + 1, j);
+        mergeRegion(board, i - 1, j);
+        mergeRegion(board, i, j + 1);
+        mergeRegion(board, i, j - 1);
+    }
 }
