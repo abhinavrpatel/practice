f3629e961c8b7a73583f956999606eb13b3cfecb
abhinavrpatel <abhinavrpatel@gmail.com>
2017-08-28 15:20:57 -0700

    map based trie

--- src/TrieAndHeap.java
+++ src/TrieAndHeap.java
@@ -1,7 +1,84 @@
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Solutions for a bunch of Trie and Heap type coding problems
  */
 public class TrieAndHeap {
 
+    // TODO Ternary Search Trie
+
+    /**
+     * Implement a trie with insert, contains, and startsWith methods.
+     */
+    public static class Trie {
+        private static class TrieNode {
+            char value;
+            Map<Character, TrieNode> children;
+            boolean isWord;
+
+            TrieNode() {
+                this.children = new HashMap<>();
+            }
+
+            TrieNode(char c) {
+                this.children = new HashMap<>();
+                this.value = c;
+            }
+        }
+
+
+        private TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            Map<Character, TrieNode> children = root.children;
+            for (int i = 0; i < word.length(); i++) {
+                char current = word.charAt(i);
+                TrieNode node;
+                if (children.containsKey(current)) {
+                    node = children.get(current);
+                } else {
+                    node = new TrieNode(current);
+                    children.put(current, node);
+                }
+                children = node.children;
+
+                if (i == word.length() - 1) {
+                    node.isWord = true;
+                }
+            }
+        }
+
+        public boolean contains(String word) {
+            TrieNode found = findNode(word);
+            return found != null && found.isWord;
+        }
+
+
+        /**
+         * Returns whether there is any word currently in the Trie that starts with the given prefix
+         */
+        public boolean startsWith(String prefix) {
+            return findNode(prefix) != null;
+        }
+
+
+        private TrieNode findNode(String word) {
+            Map<Character, TrieNode> children = root.children;
+            TrieNode current = null;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (children.containsKey(c)) {
+                    children = children.get(c).children;
+                } else {
+                    return null;
+                }
+            }
+            return current;
+        }
+    }
 }
